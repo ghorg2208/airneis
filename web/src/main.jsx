2761448f56b9bdd1a2d1ef8663c5cd907ee650bc
Azurhys <a.merpillat@h3hitema.fr>
2023-02-06 14:40:06 +0100

    hotfix footer display

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -10,10 +10,11 @@ import Contact from './composants/front/Contact';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />} />
+      <Route path='/' element={<App />}> 
         <Route path='CGU' element={<Cgu />} />
         <Route path='Mentions-Legales' element={<MentionsLegales />} />
-        <Route path='Contact' element={<Contact />} />     
+        <Route path='Contact' element={<Contact />} /> 
+      </Route>    
     </Routes>
   </BrowserRouter>
     
