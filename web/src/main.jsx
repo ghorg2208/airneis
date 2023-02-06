afc151bdfab2a2ff75007498fc08ce506f42eeb8
ghorg2208 <victor.millet92@gmail.com>
2023-02-06 15:51:24 +0100

    header

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css"
-import Menu2 from './composants/Menu2';
+import Menu from './composants/Menu';
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cgu from './composants/front/Cgu';
 import MentionsLegales from './composants/front/Mentionslegales';
@@ -16,9 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='CGU' element={<Cgu />} />
         <Route path='Mentions-Legales' element={<MentionsLegales />} />
         <Route path='Contact' element={<Contact />} />
-        <Route path='recherche' element={<Menu2 />} />
-        <Route path="panier" element={<Menu2 />} />
-        <Route path="Menu" element={<Menu2 />} /> 
+        <Route path='recherche' element={<Menu />} />
+        <Route path="panier" element={<Menu />} />
+        <Route path="Menu" element={<Menu />} /> 
       </Route>    
     </Routes>
   </BrowserRouter>
