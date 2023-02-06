fd73e805a948981e0262c5eb9c3b300a6bbe51e9
Azurhys <a.merpillat@h3hitema.fr>
2023-02-06 14:38:14 +0100

    Mentions légales

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   
 
   return (
-    <div className="page">
+    <div className="App">
       <Menu />
       <div className="container">
         <Outlet />
--- web/src/composants/front/Mentionslegales.jsx
+++ web/src/composants/front/Mentionslegales.jsx
@@ -1,7 +1,60 @@
 const MentionsLegales = () => {
-    return ( <>
-    <p>Mentions Légales</p>
-    </> );
+    return ( <div className="mx-5">
+    <h1 className="text-center">Mentions Légales</h1>
+    <h2> 1 - Édition du site </h2>
+En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://airneis.com l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:
+
+Propriétaire du site : Société AIRNEIS - Numéro de SIRET : 75221735600027 - Contact : contact@airneis.com 0799456582 - Adresse : 99 rue du peuplier 75001 Paris.
+
+Identification de l'entreprise : SAS Société AIRNEIS - Numéro de SIRET : 75221735600027 au capital social de 50000€ - SIREN : - RCS ou RM : - Adresse postale : 99 rue du peuplier 75001 Paris - [Consignes : ajoutez ici le lien hypertexte vers la page de vos conditions générales de vente si vous en avez une]
+
+Directeur de la publication : - Contact : .
+
+Hébergeur : 1&1 / IONOS - 7 Place de la Gare - 57200 Sarreguemines - Téléphone : 09.70.80.89.11
+
+Délégué à la protection des données : Antoine MERPILLAT - dpo@airneis.com
+
+Autres contributeurs :
+
+<h2>2 - Propriété intellectuelle et contrefaçons. </h2>
+Société AIRNEIS - Numéro de SIRET : 75221735600027 est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons.
+
+Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Société AIRNEIS - Numéro de SIRET : 75221735600027.
+
+Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
+
+<h2>3 - Limitations de responsabilité.</h2>
+Société AIRNEIS - Numéro de SIRET : 75221735600027 ne pourra être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site https://airneis.com.
+
+Société AIRNEIS - Numéro de SIRET : 75221735600027 décline toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur https://airneis.com.
+
+Société AIRNEIS - Numéro de SIRET : 75221735600027 s’engage à sécuriser au mieux le site https://airneis.com, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.
+
+Des espaces interactifs (espace contact ou commentaires) sont à la disposition des utilisateurs. Société AIRNEIS - Numéro de SIRET : 75221735600027 se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données.
+
+Le cas échéant, Société AIRNEIS - Numéro de SIRET : 75221735600027 se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie …).
+
+<h2>4 - CNIL et gestion des données personnelles.</h2>
+Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site https://airneis.com dispose d’un droit d’accès, de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message à notre Délégué à la Protection des Données : Antoine MERPILLAT - dpo@airneis.com.
+
+Pour plus d'informations sur la façon dont nous traitons vos données (type de données, finalité, destinataire...), lisez notre https://airneis.com/cgu. [Consignes : ajoutez ici le lien hypertexte vers votre politique de confidentialité]
+
+<h2>5 - Liens hypertextes et cookies</h2>
+Le site https://airneis.com contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers https://airneis.com.
+
+La navigation sur le site https://airneis.com est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.
+
+Un "cookie" est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les données ainsi obtenues permettent d'obtenir des mesures de fréquentation, par exemple.
+
+Vous avez la possibilité d’accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Aucun cookie ne sera déposé sans votre consentement.
+
+Les cookies sont enregistrés pour une durée maximale de 3 mois.
+
+Pour plus d'informations sur la façon dont nous faisons usage des cookies, lisez notre https://airneis.com/cgu. [Consignes : ajoutez ici le lien hypertexte vers votre politique de confidentialité ou vers votre politique de cookies si vous en avez une (c'est le cas si vous utilisez Complianz)]
+
+<h2>6 - Droit applicable et attribution de juridiction.</h2>
+Tout litige en relation avec l’utilisation du site https://airneis.com est soumis au droit français.
+    </div> );
 }
  
 export default MentionsLegales;
