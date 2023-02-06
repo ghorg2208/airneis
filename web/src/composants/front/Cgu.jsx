a4f0cdbace655183dbbc07398917e272da135336
Azurhys <a.merpillat@h3hitema.fr>
2023-02-06 14:57:30 +0100

    Mentions Légales + CGU

--- web/src/composants/front/Cgu.jsx
+++ web/src/composants/front/Cgu.jsx
@@ -1,7 +1,25 @@
 const Cgu = () => {
-    return ( <>
-    <p>CGU</p>
-    </> );
+    return ( <div className="mx-5">
+    <h1 className="text-center my-5">Conditions Générales d'Utilisation</h1>
+    Le présent document a pour objet de définir les modalités et conditions dans lesquelles d’une part,  AIRNEIS , ci-après dénommé l’EDITEUR, met à la disposition de ses utilisateurs le site, et les services disponibles sur le site et d’autre part, la manière par laquelle l’utilisateur accède au site et utilise ses services.
+
+Toute connexion au site est subordonnée au respect des présentes conditions.
+
+Pour l’utilisateur, le simple accès au site de l’EDITEUR à l’adresse URL suivante www.airneis.com  implique l’acceptation de l’ensemble des conditions décrites ci-après.
+
+<h2>Propriété intellectuelle</h2>
+
+
+La structure générale du site AIRNEIS , ainsi que les textes, graphiques, images, sons et  vidéos la composant, sont la propriété de l'éditeur ou de ses partenaires. Toute représentation et/ou reproduction et/ou exploitation partielle ou totale des contenus et services proposés par le site AIRNEIS , par quelque procédé que ce soit, sans l'autorisation préalable et par écrit de  AIRNEIS  et/ou de ses partenaires est strictement interdite et serait susceptible de constituer une contrefaçon au sens des articles L 335-2 et suivants du Code de la propriété intellectuelle.
+
+Les marques "AIRNEIS ",  sont des marques déposées par AIRNEIS int. .Toute représentation et/ou reproduction et/ou exploitation partielle ou totale de ces marques, de quelque nature que ce soit, est totalement prohibée.
+    
+<h2>Liens hypertextes</h2>
+
+Le site  AIRNEIS  peut contenir des liens hypertextes vers d’autres sites présents sur le réseau Internet. Les liens vers ces autres ressources vous font quitter le site www.airneis.com
+
+Il est possible de créer un lien vers la page de présentation de ce site sans autorisation expresse de l’EDITEUR. Aucune autorisation ou demande d’information préalable ne peut être exigée par l’éditeur à l’égard d’un site qui souhaite établir un lien vers le site de l’éditeur. Il convient toutefois d’afficher ce site dans une nouvelle fenêtre du navigateur. Cependant, l’EDITEUR se réserve le droit de demander la suppression d’un lien qu’il estime non conforme à l’objet du site AIRNEIS    
+    </div> );
 }
  
 export default Cgu;
--- web/src/composants/front/Mentionslegales.jsx
+++ web/src/composants/front/Mentionslegales.jsx
@@ -1,6 +1,6 @@
 const MentionsLegales = () => {
     return ( <div className="mx-5">
-    <h1 className="text-center">Mentions Légales</h1>
+    <h1 className="text-center my-5">Mentions Légales</h1>
     <h2> 1 - Édition du site </h2>
 En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://airneis.com l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:
 
