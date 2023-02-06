36ba9a54d53a1fbce8539bbc5d2fbcdf3eefdc6f
ghorg2208 <victor.millet92@gmail.com>
2023-02-06 15:27:42 +0100

    navbar

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Menu from './composants/Menu'
+import Menu2 from './composants/Menu2'
+//import Nav from './composants/Menu2'
 
 function App() {
   
 
   return (
     <div className="page">
-      <Menu />
+      <Menu2 />
     </div>
   )
 }
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css"
+
+import {  BrowserRouter , Routes , Route } from "react-router-dom"
+//import Home from './composant/front/Home';
+//import Contact from './composant/front/Contact';
+//import Login from './composant/front/Login';
+
+//import { AuthContextProvider } from "./context/authContext";
+import Menu2 from './composants/Menu2';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Menu2 />}>
+          <Route path='recherche' element={<Menu2 />} />
+          <Route path="panier" element={<Menu2 />} />
+          <Route path="Menu" element={<Menu2 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
 )
