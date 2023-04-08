9437f4e383d6ce5e6f7913c3788a2ba3eb49afea
Azurhys <123641752+Azurhys@users.noreply.github.com>
2023-04-08 19:26:19 +0200

    Fichiers .json samples database

--- web/src/hook/useArticle.js
+++ web/src/hook/useArticle.js
@@ -11,15 +11,13 @@ export function useArticle(){
                 if(reponse.data[key]) resultat.push({...reponse.data[key] , id : key})
             }
             setArticles(resultat)
-            //console.log(resultat);
+            console.log(resultat);
             // { "0" : { "auteur" : "Alain" , "texte" : "super article" }, "1" : { "auteur" : "Alain" , "texte" : "beau boulot" } }
             // [{ "auteur" : "Alain" , "texte" : "super article" } , ]
          })
     } , [articles.length] ) // exécute que lorsque la page est chargé et update
 
-    /* useEffect( () => { // lorsque je modifie le nombre d'article (DELETE)
-        
-    } , [])  */
+
 
     return [articles, setArticles]  ; 
 }
